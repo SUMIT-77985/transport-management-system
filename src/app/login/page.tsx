b219bee12d702a44a120394924ab82da394026e4
RoshanNaik765 <brroshan04@gmail.com>
2025-12-08 21:41:41 +0530

    link login to home page
    after login button is clicked it will take  to home page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import style from "./login.module.css";
 export default function About() {
   return (      
 <div className={style["login-container"]}>
-      <form className={style["login-box"]}>
+      <form className={style["login-box"]} action={""}>
         <h2>Login</h2>
 
         <div className={style["input-group"]}>
@@ -18,9 +18,9 @@ export default function About() {
           <label htmlFor="password">Password</label>
           <input type="password" id="password" placeholder="Enter password" />
         </div>
-
+        <Link href ="/">
         <button className={style["login-btn"]} type="submit">Login</button>
-
+        </Link>
         <p className={style["signup-text"]}>
           Don't have an account? <Link href="/signup">Sign up</Link>
         </p>
