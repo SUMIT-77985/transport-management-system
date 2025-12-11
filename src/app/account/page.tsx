a69fe17f80af866b6f644d87392aee4ead54399b
RoshanNaik765 <brroshan04@gmail.com>
2025-12-11 21:21:09 +0530

    Add account page
    account page about user account and user details

---
+++ src/app/account/page.tsx
@@ -0,0 +1,9 @@
+
+export default function account(){
+    return(
+    <div>
+        <h1>Account </h1>
+        <p>This page will display about user and services used by user.current status of services </p>
+    </div>
+    );
+}
